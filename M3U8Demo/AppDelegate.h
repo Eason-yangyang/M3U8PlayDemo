faf3c3b9e9ec3e5703da77e08d4a0a75600a3ea4
zeng yang <zywork@bytedance.com>
2020-08-10 14:47:12 +0800

    Initial Commit

---
+++ M3U8Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  M3U8Demo
+//
+//  Created by bytedance on 2020/8/10.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ M3U8Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  M3U8Demo
+//
+//  Created by bytedance on 2020/8/10.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
